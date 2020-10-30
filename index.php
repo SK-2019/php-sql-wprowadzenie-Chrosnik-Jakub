eb46b09de525590bf2fd6be4ac7d4246c33d4132
Chrosnik-Jakub <73523188+Chrosnik-Jakub@users.noreply.github.com>
2020-10-30 14:37:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -78,4 +78,23 @@ $conn= new mysqli("mysql-kuba.alwaysdata.net","kuba_ch","moj@b@z@","kuba_ch");
     echo("</tr>");
 }
     echo("</table>");
+
+
+//tabelka 5
+
+    $result=$conn->query('SELECT * FROM pracownicy where dzial = 4');
+
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+
+    while($row=$result->fetch_assoc()){
+
+    echo("<tr>");
+    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
+    echo("</tr>");
+}
+    echo("</table>");
 ?>
