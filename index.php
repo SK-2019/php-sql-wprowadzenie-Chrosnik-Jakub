e1790bc71f819f217cefd0d8c3f4447c8dee93b9
Chrosnik-Jakub <73523188+Chrosnik-Jakub@users.noreply.github.com>
2020-10-30 14:24:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,66 @@
 
 $conn= new mysqli("mysql-kuba.alwaysdata.net","kuba_ch","moj@b@z@","kuba_ch");
 
+
+//tabelka 1
+
+    $result=$conn->query('SELECT * FROM pracownicy');
+
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+
+    while($row=$result->fetch_assoc()){
+
+    echo("<tr>");
+    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
+    echo("</tr>");
+}
+    echo("</table>");
+
+
+//tabelka 2
+
+    $result=$conn->query('SELECT * FROM pracownicy');
+
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+
+    while($row=$result->fetch_assoc()){
+
+    echo("<tr>");
+    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
+    echo("</tr>");
+}
+    echo("</table>");
+
+
+//tabelka 3
+
+    $result=$conn->query('SELECT * FROM pracownicy');
+
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+
+    while($row=$result->fetch_assoc()){
+
+    echo("<tr>");
+    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
+    echo("</tr>");
+}
+    echo("</table>");
+
+
+//tabelka 4
+
     $result=$conn->query('SELECT * FROM pracownicy');
 
         echo("<table border=1>");
