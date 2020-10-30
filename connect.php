a93ae4660890153dc398ed5063068c3afcd894d9
Chrosnik-Jakub <73523188+Chrosnik-Jakub@users.noreply.github.com>
2020-10-30 15:39:37 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,16 +1,8 @@
 <?php
 
-$servername = "mysql-kuba.alwaysdata.net";
-$username = "kuba_ch";
-$password = "moj@b@z@";
-$dbanme = "kuba_ch";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
+$conn = new mysqli('mysql-kuba.alwaysdata.net','kuba_ch','moj@b@z@','kuba_ch');
 if ($conn->connect_error) {
-  die("Connection failed: ".mysqli_connect_error());
-}
+    die("connection failed: ".mysqli_connect_error());
 
+}
 ?>
