7f3ad6d134e5b43e199ff093099c8d54c177a955
Chrosnik-Jakub <73523188+Chrosnik-Jakub@users.noreply.github.com>
2020-11-05 11:03:47 +0100

    Update agregat.php

--- agregat.php
+++ agregat.php
@@ -1,3 +1,16 @@
 <?php
-echo("agregat.php")
+
+echo('<h1>Tabelka - count</h1>');
+    
+$result=$conn->query("Select count(imie) as ci, dzial, nazwa_dzial  From pracownicy, organizacja where dzial=id_org group by dzial");
+                echo("<table border=1>");
+                    echo("<th>Count(Imie)</th>");
+                    echo("<th>Dzial</th>");
+                    echo("<th>Nazwa_Dzial</th>");
+                        while($row=$result->fetch_assoc()){
+                            echo("<tr>");
+                            echo("<td>".$row["ci"]."</td><td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td>");
+                            echo("</tr>");}
+                echo("</table>");
+
 ?>
