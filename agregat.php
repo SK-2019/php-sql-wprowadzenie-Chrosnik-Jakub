1c01e63d4bd7747211a64dca1375fde033d50943
Chrosnik-Jakub <73523188+Chrosnik-Jakub@users.noreply.github.com>
2020-11-05 11:05:41 +0100

    Update agregat.php

--- agregat.php
+++ agregat.php
@@ -1,4 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
 <?php
+
+require_once("connect.php");
 
 echo('<h1>Tabelka - count</h1>');
     
