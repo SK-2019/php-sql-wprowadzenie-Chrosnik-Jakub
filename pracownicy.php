9fdc6809f558c892a4b504def679e3207d26ce4a
Chrosnik-Jakub <73523188+Chrosnik-Jakub@users.noreply.github.com>
2020-11-06 11:02:14 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -1,3 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="nav"> 
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Chrosnik-Jakub">GITHUB</a>
+     | 
+    <a href="pracownicy.php">Pracownicy</a>
+     | 
+    <a href="orgPracownicy.php">Organizacja i Pracownicy</a>
+     | 
+    <a href="agregat.php">Funkcje agregujące</a>
+</div>    
 <?php
-echo("pracownicy");
+
+require_once("connect.php");
+
+//tabelka 1
+
+    $result = $conn->query('SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE dzial = id_org and dzial = 3');
+        echo("<table border=1>");
+        echo("<th>Imie</th>");
+        echo("<th>Zarobki</th>");
+        echo("<th>Data_Urodzenia</th>");
+        echo("<th>Nazwa_Działu</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+
+                echo("</tr>");
+            }
+
+        echo("</table>");
 ?>
+</body>
+</html>
