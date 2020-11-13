3ad61168126281e8d9d025ce666cf67583cf2bc0
Chrosnik-Jakub <73523188+Chrosnik-Jakub@users.noreply.github.com>
2020-11-13 11:21:33 +0100

    Update organizacja_pracownicy.php

--- organizacja_pracownicy.php
+++ organizacja_pracownicy.php
@@ -20,84 +20,221 @@
 </div> 
 <?php
 
-require_once("connect.php");
+    require("connect.php");
+    $sql='SELECT imie, nazwa_dzial FROM pracownicy, organizacja WHERE dzial = id_org';
+    echo("<h1>Ćw 1</h1>");
+    echo("<h2>$sql</h2>");
+    $result = $conn->query($sql);
+        echo("<table border=1>");
+        echo("<th>Imie</th>");
+        echo("<th>Zarobki</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
 
-//tabelka 1
+                echo("</tr>");
+            }
 
-echo("<h1>Cwiczenie 1</h1>");
-echo("<h2>SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE dzial = id_org</h2>");
-  $result = $conn->query('SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE dzial = id_org');
-      echo("<table border=1>");
-      echo("<th>Imie</th>");
-      echo("<th>Zarobki</th>");
-      echo("<th>Data_Urodzenia</th>");
-      echo("<th>Nazwa_Działu</th>");
-          while($row=$result->fetch_assoc()){ 
-              echo("<tr>");
-                  echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+        echo("</table>");
+    require("connect.php");
+    $sql='SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) AND (dzial=1 or dzial=4)';
+    echo("<h1>Ćw 2</h1>");
+    echo("<h2>$sql</h2>");
+    $result = $conn->query($sql); 
+        echo("<table border=1>");
+        echo("<th>Imie</th>");
+        echo("<th>Zarobki</th>");
+        echo("<th>Data_Urodzenia</th>");
+        echo("<th>Nazwa_Działu</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");  
 
-              echo("</tr>");
-          }
+                echo("</tr>");
+            }
 
-      echo("</table>");
+        echo("</table>");
+    require("connect.php");
+    $sql='SELECT imie, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and (imie like "%a")';
+    echo("<h1>Ćw 3</h1>");
+    echo("<h2>$sql</h2>");
+    $result = $conn->query($sql); 
+        echo("<table border=1>");
+        echo("<th>Imie</th>");
+        echo("<th>Nazwa_Działu</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
 
-//tabelka 2
+                echo("</tr>");
+            }
 
-echo("<h1>Cwiczenie 2</h1>");
-echo("<h2>SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE dzial = id_org AND dzial = 2</h2>");
-  $result = $conn->query('SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE dzial = id_org AND dzial = 2');
-      echo("<table border=1>");
-      echo("<th>Imie</th>");
-      echo("<th>Zarobki</th>");
-      echo("<th>Data_Urodzenia</th>");
-      echo("<th>Nazwa_Działu</th>");
-          while($row=$result->fetch_assoc()){ 
-              echo("<tr>");
-                  echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+        echo("</table>");
 
-              echo("</tr>");
-          }
+        echo("</table>");
+    require("connect.php");
+    $sql='SELECT imie, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) AND (imie not like "%a")';
+    echo("<h1>Ćw 4</h1>");
+    echo("<h2>$sql</h2>");
+    $result = $conn->query($sql);
+        echo("<table border=1>");
+        echo("<th>Imie</th>");
+        echo("<th>Nazwa_Działu</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+                
+                echo("</tr>");
+            }
 
-      echo("</table>");
+        echo("</table>");
 
-//tabelka 3
+        echo("</table>");
+    require("connect.php");
+    $sql='SELECT imie, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) order by imie desc';
+    echo("<h1>Ćw 5</h1>");
+    echo("<h2>$sql</h2>"); 
+    $result = $conn->query($sql); 
+        echo("<table border=1>");
+        echo("<th>Imie</th>");
+        echo("<th>Nazwa_Działu</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                  echo("<td>".$row["imie"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
 
-echo("<h1>Cwiczenie 3</h1>");
-echo("<h2>SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE dzial = id_org AND imie like '%a'</h2>");
-  $result = $conn->query('SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE dzial = id_org AND imie like "%a"');
-      echo("<table border=1>");
-      echo("<th>Imie</th>");
-      echo("<th>Zarobki</th>");
-      echo("<th>Data_Urodzenia</th>");
-      echo("<th>Nazwa_Działu</th>");
-          while($row=$result->fetch_assoc()){ 
-              echo("<tr>");
-                  echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+                echo("</tr>");
+            }
 
-              echo("</tr>");
-          }
+        echo("</table>");
 
-      echo("</table>");
+        echo("</table>");
+    require("connect.php");
+    $sql='SELECT imie, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial=id_org) AND (dzial=3) order by imie asc';
+    echo("<h1>Ćw 6</h1>");
+    echo("<h2>$sql</h2>");
+    $result = $conn->query($sql); 
+        echo("<table border=1>");
+        echo("<th>Imie</th>");
+        echo("<th>Nazwa_Działu</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                 echo("<td>".$row["imie"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+                echo("</tr>");
+            }
 
-//tabelka 4
+        echo("</table>");
 
-echo("<h1>Cwiczenie 4</h1>");
-echo("<h2>SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE dzial = id_org AND imie not like '%a'</h2>");
-  $result = $conn->query('SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE dzial = id_org AND imie not like "%a"');
-      echo("<table border=1>");
-      echo("<th>Imie</th>");
-      echo("<th>Zarobki</th>");
-      echo("<th>Data_Urodzenia</th>");
-      echo("<th>Nazwa_Działu</th>");
-          while($row=$result->fetch_assoc()){ 
-              echo("<tr>");
-                  echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+        echo("</table>");
+        require("connect.php");
+        $sql='SELECT imie, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial=id_org) AND (imie like "%a") order by imie asc';
+        echo("<h1>Ćw 7</h1>");
+        echo("<h2>$sql</h2>");
+        $result = $conn->query($sql); 
+            echo("<table border=1>");
+            echo("<th>Imie</th>");
+            echo("<th>Nazwa_Działu</th>");
+                while($row=$result->fetch_assoc()){ 
+                    echo("<tr>");
+                        echo("<td>".$row["imie"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+                    echo("</tr>");
+                }
+    
+            echo("</table>");
 
-              echo("</tr>");
-          }
+            echo("</table>");
+        require("connect.php");
+        $sql='SELECT imie, zarobki, nazwa_dzial FROM pracownicy, organizacja where (dzial=id_org) AND (imie like "%a") AND (dzial=1 OR dzial=3) order by zarobki asc';
+        echo("<h1>Ćw 8</h1>");
+        echo("<h2>$sql</h2>");
+        $result = $conn->query($sql); 
+            echo("<table border=1>");
+            echo("<th>Imie</th>");
+            echo("<th>Zarobki</th>");
+            echo("<th>Nazwa_Działu</th>");
+                while($row=$result->fetch_assoc()){ 
+                    echo("<tr>");
+                        echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+                    echo("</tr>");
+                }
+    
+            echo("</table>");
 
-      echo("</table>");
+            echo("</table>");
+        require("connect.php");
+        $sql='SELECT imie, zarobki, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) AND (imie not like "a%") order by nazwa_dzial asc, zarobki asc';
+        echo("<h1>Ćw 9</h1>");
+        echo("<h2>$sql</h2>");
+        $result = $conn->query($sql); 
+            echo("<table border=1>");
+            echo("<th>Imie</th>");
+            echo("<th>Zarobki</th>");
+            echo("<th>Nazwa_Działu</th>");
+                while($row=$result->fetch_assoc()){ 
+                    echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+                    echo("</tr>");
+                }
+    
+            echo("</table>");
+        require("connect.php");
+        $sql='SELECT * FROM pracownicy, organizacja WHERE dzial=id_org AND dzial=4 order by zarobki desc limit 2';
+        echo("<h1>Ćw 10</h1>");
+        echo("<h2>$sql</h2>");
+        $result = $conn->query($sql); 
+            echo("<table border=1>");
+            echo("<th>Id</th>");
+            echo("<th>Imie</th>");
+            echo("<th>Dział</th>");
+            echo("<th>Nazwa_działu</th>");
+            echo("<th>Zarobki</th>");
+            echo("<th>Data_urodzenia</th>");
+                while($row=$result->fetch_assoc()){ 
+                    echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>"); 
+                    echo("</tr>");
+                }
+    
+            echo("</table>");
+        require("connect.php");
+        $sql='SELECT * FROM pracownicy, organizacja WHERE dzial=id_org AND (dzial=4 or dzial=2) order by zarobki desc limit 3';
+        echo("<h1>Ćw 11</h1>");
+        echo("<h2>$sql</h2>");
+        $result = $conn->query($sql); 
+            echo("<table border=1>");
+            echo("<th>Id</th>");
+            echo("<th>Imie</th>");
+            echo("<th>Dział</th>");
+            echo("<th>Nazwa_działu</th>");
+            echo("<th>Zarobki</th>");
+            echo("<th>Data_urodzenia</th>");
+                while($row=$result->fetch_assoc()){ 
+                    echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>"); 
+                    echo("</tr>");
+                }
+    
+            echo("</table>");
+        require("connect.php");
+        $sql='SELECT * FROM pracownicy, organizacja WHERE dzial=id_org order by data_urodzenia asc limit 1';
+        echo("<h1>Ćw 12</h1>");
+        echo("<h2>$sql</h2>");
+        $result = $conn->query($sql); 
+            echo("<table border=1>");
+            echo("<th>Id</th>");
+            echo("<th>Imie</th>");
+            echo("<th>Dział</th>");
+            echo("<th>Nazwa_działu</th>");
+            echo("<th>Zarobki</th>");
+            echo("<th>Data_urodzenia</th>");
+                while($row=$result->fetch_assoc()){ 
+                    echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>"); 
+                    echo("</tr>");
+                }
+    
+            echo("</table>");
 
 ?>
+    
 </body>
-</html>-
+</html>
