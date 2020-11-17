09a0e4bbf258978352e3b3981b6f959f40078804
Chrosnik-Jakub <73523188+Chrosnik-Jakub@users.noreply.github.com>
2020-11-17 10:10:19 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="nav"> 
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Chrosnik-Jakub">GITHUB</a>
+     | 
+    <a href="pracownicy.php">Pracownicy</a>
+     | 
+    <a href="organizacja_pracownicy.php">Organizacja i Pracownicy</a>
+     | 
+    <a href="agregat.php">Funkcje agregujące</a>
+     | 
+    <a href="data i czas.php">Data i Czas</a>
+</div>
+
 <?php
 
 echo ("strona.php");
@@ -18,3 +39,5 @@ echo ($_POST['zarobki']);
 echo ($_POST['Data_urodzenia']);
 
 ?>
+</body>
+</html>
