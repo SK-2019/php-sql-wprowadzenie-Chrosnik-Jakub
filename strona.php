9d32528928a6b682e54cd13a7c8ce2dbc3235e6d
Chrosnik-Jakub <73523188+Chrosnik-Jakub@users.noreply.github.com>
2020-11-17 10:01:13 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,6 +1,15 @@
 <?php
 echo ("strona.php");
 
-echo ($_POST['name']);
+echo ($_POST['id']);
+echo ("</br>");
+echo ($_POST['imie']);
+echo ("</br>");
+echo ($_POST['dzial']);
+echo ("</br>");
+echo ($_POST['zarobki']);
+echo ("</br>");
+echo ($_POST['Data_urodzenia']);
+
 
 ?>
