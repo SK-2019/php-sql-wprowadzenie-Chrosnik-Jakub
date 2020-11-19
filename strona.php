16f04d53e297ee574c8ce5d446cd1f7b9669948d
Chrosnik-Jakub <73523188+Chrosnik-Jakub@users.noreply.github.com>
2020-11-19 11:06:07 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -10,6 +10,8 @@
 <div class="nav"> 
     <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Chrosnik-Jakub">GITHUB</a>
      | 
+    <a href="index.php">Index</a>
+     | 
     <a href="pracownicy.php">Pracownicy</a>
      | 
     <a href="organizacja_pracownicy.php">Organizacja i Pracownicy</a>
@@ -17,7 +19,9 @@
     <a href="agregat.php">Funkcje agregujące</a>
      | 
     <a href="data i czas.php">Data i Czas</a>
-</div>
+     | 
+    <a href="formularz.html">Formularz</a>
+</div> 
 
 <?php
     
