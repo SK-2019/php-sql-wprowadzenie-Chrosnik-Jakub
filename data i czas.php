978cad4e2e5d5e05fc2375997a7d4202bc68caf3
Chrosnik-Jakub <73523188+Chrosnik-Jakub@users.noreply.github.com>
2020-11-19 11:06:53 +0100

    Update data i czas.php

--- data i czas.php
+++ data i czas.php
@@ -10,6 +10,8 @@
 <div class="nav"> 
     <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Chrosnik-Jakub">GITHUB</a>
      | 
+    <a href="index.php">Index</a>
+     | 
     <a href="pracownicy.php">Pracownicy</a>
      | 
     <a href="organizacja_pracownicy.php">Organizacja i Pracownicy</a>
@@ -17,6 +19,8 @@
     <a href="agregat.php">Funkcje agregujące</a>
      | 
     <a href="data i czas.php">Data i Czas</a>
+     | 
+    <a href="formularz.html">Formularz</a>
 </div> 
     
 <?php
