211b1a8c769fc53a716c993df1f0816202263ef6
Chrosnik-Jakub <73523188+Chrosnik-Jakub@users.noreply.github.com>
2020-11-20 15:26:54 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,13 +1,20 @@
 <?php
-echo("jestes w insert.php");
-echo $_POST['name'];
 
-require("connect.php");
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
 
-$sql = "INSERT INTO pracownicy (id_pracownicy,imie, dzial, zarobki, data_urodzenia) 
-       VALUES (NULL,".$_POST['imie'].", ".$_POST['dzial'], ".$_POST['zarobki'], ".$_POST['data_urodzenia'])";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
-echo "<li>".$sql;
+$sql = "INSERT INTO Pracownik (null, imie, dzial, zarobki) 
+       VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
+
+//zapisanie do bazy danych
+$conn->query($sql);
 
 $conn->close();
 ?>
