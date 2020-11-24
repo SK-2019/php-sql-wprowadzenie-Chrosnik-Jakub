4dfe997da2cd11426e791eeb143ad4bdfe84dc81
Chrosnik-Jakub <73523188+Chrosnik-Jakub@users.noreply.github.com>
2020-11-24 09:12:15 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,4 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="nav"> 
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Chrosnik-Jakub">GITHUB</a>
+     | 
+    <a href="index.php">Index</a>
+     | 
+    <a href="pracownicy.php">Pracownicy</a>
+     | 
+    <a href="organizacja_pracownicy.php">Organizacja i Pracownicy</a>
+     | 
+    <a href="agregat.php">Funkcje agregujące</a>
+     | 
+    <a href="data i czas.php">Data i Czas</a>
+     | 
+    <a href="formularz.html">Formularz</a>
+</div> 
+</body>
+
 <?php
+
+echo("jestes w insert.php");
+echo "<li>". $_POST['name'];
+echo "<li>". $_POST['dzial'];
+echo "<li>". $_POST['zarobki'];
+echo "<li>". $_POST['data_urodzenia'];
 
 $servername = "mysql-kuba.alwaysdata.net";
 $username = "kuba_ch";
@@ -10,14 +42,19 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-echo("jestes w insert.php");
-echo $_POST['name'];
+$sql = "INSERT INTO pracownicy (id_pracownicy,imie, dzial, zarobki, data_urodzenia) 
+       VALUES (null,'".$_POST['name']."', ".$_POST['dzial'].", ".$_POST['zarobki'].",'".$_POST['data_urodzenia']."')";
 
-$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki) 
-       VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
 
-//zapisanie do bazy danych
-$conn->query($sql);
+echo "<li>". $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo ("New record created successfully");
+} else {
+//informacja o ewentualnych błędach
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 $conn->close();
 ?>
+</html>
