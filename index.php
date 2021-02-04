66f9ce94d667ac72e6a5b8746ed7ea5d01d9f796
Chrosnik Jakub <jakubchrosnik@gmail.com>
2021-02-04 10:58:43 +0100

    .

--- index.php
+++ index.php
@@ -17,21 +17,21 @@
 
 <div class="colorBlue">
     <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Chrosnik-Jakub">GITHUB</a>
-     | 
+
     <a href="/index.php">Index</a>
-     | 
+
     <a href="/Pracownicy/pracownicy.php">Pracownicy</a>
-     | 
+
     <a href="/Pracownicy/organizacja_pracownicy.php">Organizacja i Pracownicy</a>
-     | 
+
     <a href="/Pracownicy/agregat.php">Funkcje agregujące</a>
-     | 
+
     <a href="/Pracownicy/data i czas.php">Data i Czas</a>
-     | 
+
     <a href="/Formularz/formularz.html">Formularz</a>
-     | 
+
     <a href="/Formularz/daneDoBazy.php">Dane do bazy</a>
-     |
+
     <a href="/Biblioteka/ksiazki.php">Ksiazki</a>
 </div> 
 </body>
