886e3c118c21c85f92028c24b8d93213b8280d14
Chrosnik-Jakub <jakubchrosnik@gmail.com>
2021-02-28 20:59:54 +0100

    a

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    &lt;link rel="shortcut icon" href="/favicon.ico"/&gt;
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
 </head>
