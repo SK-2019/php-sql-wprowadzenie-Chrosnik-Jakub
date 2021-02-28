9f4d90e3568a0250de42475e2a1d9ee18cf57437
Chrosnik-Jakub <73523188+Chrosnik-Jakub@users.noreply.github.com>
2021-02-28 18:25:52 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 //require_once("config.php");
-$conn = new mysqli($_SERVER['SERV'],$_SERVER['USER'], $_SERVER['PASS'], $_SERVER['DB']);
+$conn = new mysqli($_SERVER['SERV'],$_SERVER['NAME'], $_SERVER['PASS'], $_SERVER['DB']);
 if ($conn->connect_error) {
     die("connection failed: ".mysqli_connect_error());
 
