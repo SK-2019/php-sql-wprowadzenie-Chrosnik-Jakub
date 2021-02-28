5401a90e7c64b1233a1c0c3cef41a4d15e80ae8c
Chrosnik-Jakub <jakubchrosnik@gmail.com>
2021-02-28 19:19:54 +0100

    class

--- Pracownicy/agregat.php
+++ Pracownicy/agregat.php
@@ -10,23 +10,23 @@
 <div class="colorRed">
 </div>
 <div class="colorBlue"> 
-    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Chrosnik-Jakub">GITHUB</a>
-     | 
-    <a href="/index.php">Index</a>
-     | 
-    <a href="/Pracownicy/pracownicy.php">Pracownicy</a>
-     | 
-    <a href="/Pracownicy/organizacja_pracownicy.php">Organizacja i Pracownicy</a>
-     | 
-    <a href="/Pracownicy/agregat.php">Funkcje agregujące</a>
-     | 
-    <a href="/Pracownicy/data i czas.php">Data i Czas</a>
-     | 
-    <a href="/Formularz/formularz.html">Formularz</a>
-     | 
-    <a href="/Formularz/daneDoBazy.php">Dane do bazy</a>
-     |
-    <a href="/Biblioteka/ksiazki.php">Ksiazki</a>
+<a class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-Chrosnik-Jakub">GITHUB</a>
+
+<a class="link" href="/index.php">Index</a>
+
+<a class="link" href="/Pracownicy/pracownicy.php">Pracownicy</a>
+
+<a class="link" href="/Pracownicy/organizacja_pracownicy.php">Organizacja i Pracownicy</a>
+
+<a class="link" href="/Pracownicy/agregat.php">Funkcje agregujące</a>
+
+<a class="link" href="/Pracownicy/data i czas.php">Data i Czas</a>
+
+<a class="link" href="/Formularz/formularz.html">Formularz</a>
+
+<a class="link" href="/Formularz/daneDoBazy.php">Dane do bazy</a>
+
+<a class="link" href="/Biblioteka/ksiazki.php">Ksiazki</a>
 </div> 
 <div class="colorGreen"> 
 <?php
